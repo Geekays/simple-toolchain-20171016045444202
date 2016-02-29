3c21f6813096af1060740f5333cfd693bcf7f63a
gEEKAYS <geekays@gmail.com>
2016-02-29 23:13:57 +0530

    Updated JUnit tests

--- src/test/java/com/example/ConverterTester.java
+++ src/test/java/com/example/ConverterTester.java
@@ -5,9 +5,22 @@ import static org.junit.Assert.assertEquals;
 import org.junit.*;
 
 public class ConverterTester {
+	static TempConverter _tc;
+	
+	//execute before class
+	@BeforeClass
+	public static void before() {
+	   System.out.println("in before class");
+	   _tc = new TempConverter();
+	}
 
 	@Test
 	public void CtoFTest(){
-		assertEquals("100 C = 212 F : ", 1, 1);
+		assertEquals("100 C = 212 F : ", 212, _tc.CtoF(212));
+	}
+
+	@Test
+	public void FtoCTest(){
+		assertEquals("212 F = 100 C : ", 100, _tc.FtoC(212));
 	}
 }
