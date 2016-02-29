c086df2933078adfd59c9502c2c58e7610e9fb08
gEEKAYS <geekays@gmail.com>
2016-02-29 22:52:53 +0530

    Added unit tests

---
+++ src/test/java/com/example/ConverterTester.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+public class ConverterTester {
+
+	@Test
+	public void CtoFTest(){
+		assertEquals("100 C = 212 F : ", 1, 1);
+	}
+}
