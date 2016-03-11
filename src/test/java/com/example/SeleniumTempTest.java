0e8a9f6820ac0e7da4a5c9aa585652e64618c776
gEEKAYS <geekays@gmail.com>
2016-03-11 13:54:54 +0530

    Updated Selenium Tests

--- src/test/java/com/example/SeleniumTempTest.java
+++ src/test/java/com/example/SeleniumTempTest.java
@@ -6,9 +6,51 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 
 public class SeleniumTempTest {
+	
+	private FirefoxProfile profile;
+	private WebDriver driver;
+	String appURL="http://java-web-service.au-syd.mybluemix.net/api/";
+
+	@BeforeClass
+	public void TestSetUp(){
+		profile = new FirefoxProfile();
+		driver = new FirefoxDriver(profile);
+	}
+
+	@Test
+	public void verifyCtoF() {
+		//driver.navigate().to(appURL);
+		//String getTitle = driver.getTitle();
+		//Assert.assertEquals(getTitle, "Google");
+		appURL = appURL+"/ctof/100";
+		driver.get(appURL);
+		driver.manage().window().maximize();
+		System.out.println("11111111");
+		WebElement result=driver.findElement(By.xpath("/html/body/pre"));
+		
+		String val=result.getText();
+		System.out.println("Done"+val+"url:"+appURL.substring(54, 57)+":val==="+val.substring(10, 13));
+		
+		if((appURL.substring(54, 57)).equals("100") ){
+			System.out.println("1111111111");
+			if((val.substring(10, 13)).equals("212")){
+				System.out.println("222222222");
+				System.out.println("Value Pass");
+			}
+		}
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("START MAIN");
